e9d8aea2900ed4482f17fb347538b25b63d0bcaa
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2023-03-19 17:00:53 +0530

    .

--- TicTacToe_GUI_code.py
+++ TicTacToe_GUI_code.py
@@ -3,7 +3,7 @@ from kivy.lang import Builder
 from kivy.uix.popup import Popup
 
 KV = ("""
-<URLPopup>:
+<ResultPopup>:
     size_hint: .5,.5
     title: 'Result'
     auto_dismiss: False
@@ -149,7 +149,7 @@ MDScreen:
 Turn = 'X'
 nextP = False
 
-class URLPopup(Popup):
+class ResultPopup(Popup):
     pass
 
 class MainApp(MDApp):
@@ -175,12 +175,12 @@ class MainApp(MDApp):
 		winCases = [[1,2,3],[1,7,4],[4,6,5],[7,8,9],[2,5,8],[3,9,6],[1,5,9],[3,5,7]]
 		for i in range(8):
 			if self.root.ids[f"b{winCases[i][0]}"].text == self.root.ids[f"b{winCases[i][1]}"].text and self.root.ids[f"b{winCases[i][0]}"].text == self.root.ids[f"b{winCases[i][2]}"].text and self.root.ids[f"b{winCases[i][1]}"].text == self.root.ids[f"b{winCases[i][2]}"].text and self.root.ids[f"b{winCases[i][0]}"].text in ['X','O'] and self.root.ids[f"b{winCases[i][1]}"].text in ['X','O'] and self.root.ids[f"b{winCases[i][2]}"].text in ['X','O']:
-				popup = URLPopup()
+				popup = ResultPopup()
 				popup.ids.result.text = f'{self.root.ids[f"b{winCases[i][0]}"].text} wins !'
 				popup.open()
 				self.clearWindow()
 			elif self.root.ids.b1.text in ['X','O'] and self.root.ids.b2.text in ['X','O'] and self.root.ids.b3.text in ['X','O'] and self.root.ids.b4.text in ['X','O'] and self.root.ids.b5.text in ['X','O'] and self.root.ids.b6.text in ['X','O'] and self.root.ids.b7.text in ['X','O'] and self.root.ids.b8.text in ['X','O'] and self.root.ids.b9.text in ['X','O']:
-				popup = URLPopup()
+				popup = ResultPopup()
 				popup.ids.result.text = f"Draw !"
 				popup.open()
 				self.clearWindow()
