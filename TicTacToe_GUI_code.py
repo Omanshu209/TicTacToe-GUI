255cdeed17704865d58f8fdffc0d2c7b4527d657
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2023-03-19 16:55:42 +0530

    .

--- TicTacToe_GUI_code.py
+++ TicTacToe_GUI_code.py
@@ -161,112 +161,29 @@ class MainApp(MDApp):
 		
 	def displayXO(self,b):
 		global Turn,nextP
-		if b == 1:
-			if self.root.ids.b1.text not in ['X','O']:
-				self.root.ids.b1.text = Turn
-				nextP = True
-			else:
-				nextP = False
-		elif b == 2:
-			if self.root.ids.b2.text not in ['X','O']:
-				self.root.ids.b2.text = Turn
-				nextP = True
-			else:
-				nextP = False
-		elif b == 3:
-			if self.root.ids.b3.text not in ['X','O']:
-				self.root.ids.b3.text = Turn
-				nextP = True
-			else:
-				nextP = False
-		elif b == 4:
-			if self.root.ids.b4.text not in ['X','O']:
-				self.root.ids.b4.text = Turn
-				nextP = True
-			else:
-				nextP = False
-		elif b == 5:
-			if self.root.ids.b5.text not in ['X','O']:
-				self.root.ids.b5.text = Turn
-				nextP = True
-			else:
-				nextP = False
-		elif b == 6:
-			if self.root.ids.b6.text not in ['X','O']:
-				self.root.ids.b6.text = Turn
-				nextP = True
-			else:
-				nextP = False
-		elif b == 7:
-			if self.root.ids.b7.text not in ['X','O']:
-				self.root.ids.b7.text = Turn
-				nextP = True
-			else:
-				nextP = False
-		elif b == 8:
-			if self.root.ids.b8.text not in ['X','O']:
-				self.root.ids.b8.text = Turn
-				nextP = True
-			else:
-				nextP = False
-		elif b == 9:
-			if self.root.ids.b9.text not in ['X','O']:
-				self.root.ids.b9.text = Turn
-				nextP = True
-			else:
-				nextP = False
+		if self.root.ids[f"b{b}"].text not in ['X','O']:
+			self.root.ids[f"b{b}"].text = Turn
+			nextP = True
+		else:
+			nextP = False
 		if Turn == 'X' and nextP == True:
 			Turn = 'O'
 		elif Turn == 'O' and nextP == True:
 			Turn = 'X'
 			
 	def checkResult(self):
-		if self.root.ids.b1.text == self.root.ids.b2.text and self.root.ids.b1.text == self.root.ids.b3.text and self.root.ids.b3.text == self.root.ids.b2.text and self.root.ids.b1.text in ['X','O'] and self.root.ids.b2.text in ['X','O'] and self.root.ids.b3.text in ['X','O']:
-			popup = URLPopup()
-			popup.ids.result.text = f"{self.root.ids.b1.text} wins !"
-			popup.open()
-			self.clearWindow()
-		elif self.root.ids.b1.text == self.root.ids.b4.text and self.root.ids.b1.text == self.root.ids.b7.text and self.root.ids.b7.text == self.root.ids.b4.text and self.root.ids.b1.text in ['X','O'] and self.root.ids.b4.text in ['X','O'] and self.root.ids.b7.text in ['X','O']:
-			popup = URLPopup()
-			popup.ids.result.text = f"{self.root.ids.b1.text} wins !"
-			popup.open()
-			self.clearWindow()
-		elif self.root.ids.b4.text == self.root.ids.b5.text and self.root.ids.b4.text == self.root.ids.b6.text and self.root.ids.b5.text == self.root.ids.b6.text and self.root.ids.b4.text in ['X','O'] and self.root.ids.b5.text in ['X','O'] and self.root.ids.b6.text in ['X','O']:
-			popup = URLPopup()
-			popup.ids.result.text = f"{self.root.ids.b4.text} wins !"
-			popup.open()
-			self.clearWindow()
-		elif self.root.ids.b7.text == self.root.ids.b8.text and self.root.ids.b7.text == self.root.ids.b9.text and self.root.ids.b8.text == self.root.ids.b9.text and self.root.ids.b7.text in ['X','O'] and self.root.ids.b8.text in ['X','O'] and self.root.ids.b9.text in ['X','O']:
-			popup = URLPopup()
-			popup.ids.result.text = f"{self.root.ids.b7.text} wins !"
-			popup.open()
-			self.clearWindow()
-		elif self.root.ids.b2.text == self.root.ids.b5.text and self.root.ids.b2.text == self.root.ids.b8.text and self.root.ids.b5.text == self.root.ids.b8.text and self.root.ids.b2.text in ['X','O'] and self.root.ids.b5.text in ['X','O'] and self.root.ids.b8.text in ['X','O']:
-			popup = URLPopup()
-			popup.ids.result.text = f"{self.root.ids.b2.text} wins !"
-			popup.open()
-			self.clearWindow()
-		elif self.root.ids.b3.text == self.root.ids.b6.text and self.root.ids.b3.text == self.root.ids.b9.text and self.root.ids.b6.text == self.root.ids.b9.text  and self.root.ids.b6.text in ['X','O'] and self.root.ids.b9.text in ['X','O'] and self.root.ids.b3.text in ['X','O']:
-			popup = URLPopup()
-			popup.ids.result.text = f"{self.root.ids.b3.text} wins !"
-			popup.open()
-			self.clearWindow()
-		elif self.root.ids.b1.text == self.root.ids.b5.text and self.root.ids.b9.text == self.root.ids.b1.text and self.root.ids.b5.text == self.root.ids.b9.text and self.root.ids.b1.text in ['X','O'] and self.root.ids.b5.text in ['X','O'] and self.root.ids.b9.text in ['X','O']:
-			popup = URLPopup()
-			popup.ids.result.text = f"{self.root.ids.b1.text} wins !"
-			popup.open()
-			self.clearWindow()
-		elif self.root.ids.b7.text == self.root.ids.b5.text and self.root.ids.b7.text == self.root.ids.b3.text and self.root.ids.b5.text == self.root.ids.b3.text and self.root.ids.b7.text in ['X','O'] and self.root.ids.b5.text in ['X','O'] and self.root.ids.b3.text in ['X','O']:
-			popup = URLPopup()
-			popup.ids.result.text = f"{self.root.ids.b7.text} wins !"
-			popup.open()
-			self.clearWindow()
-		#elif self.root.ids.b1.text != '' and self.root.ids.b2.text != '' and self.root.ids.b3.text != '' and self.root.ids.b4.text != '' and self.root.ids.b5.text != '' and self.root.ids.b6.text != '' and self.root.ids.b7.text != '' and self.root.ids.b8.text != '' and self.root.ids.b9.text != '':
-		elif self.root.ids.b1.text in ['X','O'] and self.root.ids.b2.text in ['X','O'] and self.root.ids.b3.text in ['X','O'] and self.root.ids.b4.text in ['X','O'] and self.root.ids.b5.text in ['X','O'] and self.root.ids.b6.text in ['X','O'] and self.root.ids.b7.text in ['X','O'] and self.root.ids.b8.text in ['X','O'] and self.root.ids.b9.text in ['X','O']:
-			popup = URLPopup()
-			popup.ids.result.text = f"Draw !"
-			popup.open()
-			self.clearWindow()
+		winCases = [[1,2,3],[1,7,4],[4,6,5],[7,8,9],[2,5,8],[3,9,6],[1,5,9],[3,5,7]]
+		for i in range(8):
+			if self.root.ids[f"b{winCases[i][0]}"].text == self.root.ids[f"b{winCases[i][1]}"].text and self.root.ids[f"b{winCases[i][0]}"].text == self.root.ids[f"b{winCases[i][2]}"].text and self.root.ids[f"b{winCases[i][1]}"].text == self.root.ids[f"b{winCases[i][2]}"].text and self.root.ids[f"b{winCases[i][0]}"].text in ['X','O'] and self.root.ids[f"b{winCases[i][1]}"].text in ['X','O'] and self.root.ids[f"b{winCases[i][2]}"].text in ['X','O']:
+				popup = URLPopup()
+				popup.ids.result.text = f'{self.root.ids[f"b{winCases[i][0]}"].text} wins !'
+				popup.open()
+				self.clearWindow()
+			elif self.root.ids.b1.text in ['X','O'] and self.root.ids.b2.text in ['X','O'] and self.root.ids.b3.text in ['X','O'] and self.root.ids.b4.text in ['X','O'] and self.root.ids.b5.text in ['X','O'] and self.root.ids.b6.text in ['X','O'] and self.root.ids.b7.text in ['X','O'] and self.root.ids.b8.text in ['X','O'] and self.root.ids.b9.text in ['X','O']:
+				popup = URLPopup()
+				popup.ids.result.text = f"Draw !"
+				popup.open()
+				self.clearWindow()
 			
 	def clearWindow(self):
 		self.root.ids.b1.text=""
